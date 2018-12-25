a2ae49b48b3e318eabb0ab85177f4fdfdfe8ce14
TaaJackmangit <41802549+TaaJackmangit@users.noreply.github.com>
2018-12-26 00:31:22 +0530

    Add files via upload

---
+++ src/main/java/com/nit/heroku/SpringbootHerokuExampleApplication.java
@@ -0,0 +1,28 @@
+package com.nit.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootHerokuExampleApplication {
+
+	@GetMapping("/")
+	public String sayHi() {
+		return "Welcome to Heroku Cloud solution";
+	}
+	
+	@GetMapping("/message/{input}")
+	public String getMessage(@PathVariable String input) {
+		return "Welcome " + input;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHerokuExampleApplication.class, args);
+	}
+
+}
+
